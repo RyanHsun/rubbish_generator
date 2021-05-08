c9fa48070b827c34e2e2ee34418c6995ea7d8032
Ryan Hsun <lenxun.huang@gmail.com>
2021-05-09 01:40:38 +0800

    feat: update app.js function

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static('public'))
 
 // 設定路由
 app.get('/', (req, res) => {
-  res.render('index', { targets: targetList.results })
+  res.render('index')
 })
 
 app.post('/', (req, res) => {
